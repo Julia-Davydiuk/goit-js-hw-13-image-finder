4ae6f659ff09fb00869d9386f1b42f63f74d1210
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2021-03-24 11:07:07 +0100

    исправила ошибки

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import templates from './templates/templates.hbs';
 import apiService from './apiService.js';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import * as basicLightbox from 'basiclightbox'
-import { alert, defaultModules } from '@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
-import '@pnotify/core/dist/BrightTheme.css';
-import 'material-design-icons/iconfont/material-icons.css';
-
-defaultModules.set(PNotifyMobile, {});
 
 const galleryRef = document.querySelector('.gallery');
 const formRef = document.querySelector('.search-form');
@@ -21,37 +15,37 @@ function searchForm(event) {
   event.preventDefault();
   apiService.query = event.currentTarget.elements.query.value;
   galleryRef.innerHTML = '';
-  apiService.resetPage();
+  loadMoreBtn.classList.add('is-hidden');
   onImg();
 }
 
 function onImg() {
-  apiService.fetchImages().then(images => {
-    if (images.length === 0) {
-      alert({
-        text: 'No matches found! Try again',
-        type: 'error',
-        delay: 2000,
-        stack: new Stack({
-          dir1: 'up',
-        }),
-      });
-    }
-    galleryRef.insertAdjacentHTML('beforeend', templates(images));
+  apiService
+  .fetchImages()
+  .then(images => {
+    if(images.length > 10) loadMoreBtn.classList.remove('is-hidden');
+   galleryRef.insertAdjacentHTML('beforeend', templates(images));
   });
+}
+
+function scrollToNewElements() {
+  const totalScrollHeight = galleryRef.clientHeight + 80
+  setTimeout(() => {
+    window.scrollTo({
+      top: totalScrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
 }
 
 loadMoreBtn.addEventListener('click', event => {
   apiService
     .fetchImages()
     .then(onImg)
-    .then(() => {
-      window.scrollTo(0, galleryRef.offsetHeight);
-    });
+    .then(scrollToNewElements);
 });
 
 function openModal(event) {
-  event.preventDefault();
   if (event.target.nodeName !== 'IMG') {
     return;
   }
