af959b903329c3fdcccf88fead5e8d9dbf8556dc
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2021-04-04 22:03:47 +0200

    бесконечный скрол

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as basicLightbox from 'basiclightbox'
 
 const galleryRef = document.querySelector('.gallery');
 const formRef = document.querySelector('.search-form');
-const loadMoreBtn = document.querySelector('.gallery-btn')
+const sentinelRef = document.querySelector('#sentinel');
 
 formRef.addEventListener('submit', searchForm)
 galleryRef.addEventListener('click', openModal)
@@ -15,35 +15,31 @@ function searchForm(event) {
   event.preventDefault();
   apiService.query = event.currentTarget.elements.query.value;
   galleryRef.innerHTML = '';
-  loadMoreBtn.classList.add('is-hidden');
   onImg();
 }
 
 function onImg() {
   apiService
   .fetchImages()
-  .then(images => {
-    if(images.length > 10) loadMoreBtn.classList.remove('is-hidden');
-   galleryRef.insertAdjacentHTML('beforeend', templates(images));
-  })
+  .then(images => 
+    galleryRef.insertAdjacentHTML('beforeend', templates(images))
+    )
 }
 
-loadMoreBtn.addEventListener('click', event => {
-  apiService
-    .fetchImages()
-    .then(onImg)
-    .then(scrollToNewElements);
-});
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting && apiService.query !== '') {
+     onImg();
+    }
+  });
+};
 
-function scrollToNewElements() {
-  const totalScrollHeight = galleryRef.clientHeight + 80;
-  setTimeout(() => {
-    window.scrollTo({
-      top: totalScrollHeight,
-      behavior: 'smooth',
-    });
-  }, 100);
-}
+const options = {
+  rootMargin: '150px'
+};
+
+const observer = new IntersectionObserver(onEntry, options)
+observer.observe(sentinelRef);
 
 function openModal(event) {
   if (event.target.nodeName !== 'IMG') {
